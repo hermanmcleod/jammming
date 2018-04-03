65c02a7393f83124b7b5eb65849130760702016d
Herman McLeod <hermanmcleod1@gmail.com>
2018-04-02 22:13:13 -0400

    to step 73 Add spotify searchbar

--- src/components/playlist/Playlist.js
+++ src/components/playlist/Playlist.js
@@ -27,7 +27,7 @@ class Playlist extends React.Component {
   				onRemove={this.props.removeTrack}
   				onChange={this.handleNameChange}
   				/>
-  				<a className="Playlist-save">SAVE TO SPOTIFY</a>
+  				<a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
 			</div>
 			);
 	};
